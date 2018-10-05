0a8d992ed6325760144b8a70bb20a08553b2c384
Hannah Q <qiuhhan@berkeley.edu>
2018-10-05 13:47:15 -0700

    made change in *teams*

--- workout01/code/make-teams-table.R
+++ workout01/code/make-teams-table.R
@@ -55,7 +55,20 @@ sink()
 
 
 #Creating nba2018-teams.csv
-teams = data.frame(summarise(group_by(nba2018, team), experience = round(sum(experience),2), salary = round(sum(salary),2), points3 = round(sum(points3),2), points2 = round(sum(points2),2), points1 = round(sum(points1),2), off_rebounds = round(sum(off_rebounds),2), def_rebounds = round(sum(off_rebounds),2), assists = round(sum(assists),2), steals = round(sum(steals),2), blocks = round(sum(blocks),2), turnovers = round(sum(turnovers),2), fouls = round(sum(fouls),2), efficiency = round(sum(efficiency),2)))
+teams = data.frame(summarise(group_by(nba2018, team), 
+                             experience = round(sum(experience),2), 
+                             salary = round(sum(salary),2), 
+                             points3 = round(sum(points3),2), 
+                             points2 = round(sum(points2),2), 
+                             points1 = round(sum(points1),2), 
+                             off_rebounds = round(sum(off_rebounds),2), 
+                             def_rebounds = round(sum(off_rebounds),2), 
+                             assists = round(sum(assists),2), 
+                             steals = round(sum(steals),2), 
+                             blocks = round(sum(blocks),2), 
+                             turnovers = round(sum(turnovers),2), 
+                             fouls = round(sum(fouls),2), 
+                             efficiency = round(sum(efficiency),2)))
 
 sink(file = '../data/teams-summary.txt')
 summary(teams)

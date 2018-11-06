42114e14179bd72f212e54f55ffa6256c4905d64
Hannah Q <qiuhhan@berkeley.edu>
2018-11-05 18:11:16 -0800

    updated

--- workout02/code/functions/count-vowels.R
+++ workout02/code/functions/count-vowels.R
@@ -6,42 +6,17 @@
 library(stringr)
 
 count_vowels = function(x) {
-  num = nchar(x)
-  lower = tolower(x)
-  vowels = c('a', 'e', 'i', 'o', 'u')
-  split = str_split(as.vector(lower), "")
-  
-  j = 1
-  a = 0
-  e = 0
-  i = 0
-  o = 0
-  u = 0
-  
-  if (typeof(x) != "character") {
-    stop("ERROR")
+  if ((typeof(x) != "character") & (length(x) == 1)) {
+    stop("invalid input; a string was expected")
   } else {
-    while (j < num + 1) {
-      if (split[[1]][j] == vowels[1]) {
-        a = a + 1
-        j = j + 1
-      } else if (split[[1]][j] == vowels[2]) {
-        e = e + 1
-        j = j + 1
-      } else if (split[[1]][j] == vowels[3]) {
-        i = i + 1
-        j = j + 1
-      } else if (split[[1]][j] == vowels[4]) {
-        o = o + 1
-        j = j + 1
-      } else if (split[[1]][j] == vowels[5]) {
-        u = u + 1
-        j = j + 1
-      } else {
-        j = j + 1
-      }
-    }
-    count = c(a, e, i, o, u)
-    return(count)
+    x = tolower(x)
+    counts = c(str_count(x, pattern = "a"),
+               str_count(x, pattern = "e"),
+               str_count(x, pattern = "i"),
+               str_count(x, pattern = "o"),
+               str_count(x, "u"))
+    counts = as.double(counts)
+    names(counts) = c("a","e","i","o","u")
   }
+  return(counts)
 }
--- workout02/code/functions/hex-color.R
+++ workout02/code/functions/hex-color.R
@@ -1,35 +1,38 @@
 #' @title is_hex()
 #' @description checks whether an input string is a valid color in
 #' hexadecimal notation without an alpha transparency
-#' @param x a string
+#' @param hex a string
 #' @return TRUE or FALSE
 
 library(stringr)
 
 is_hex = function(hex) {
   num = nchar(hex)
-  hex_split = str_split(as.vector(tolower(hex)), "")
+  hex2 = as.vector(tolower(hex))
   if (typeof(hex) != "character") {
     stop("invalid input; a string was expected")
-  } else if (num == 7 & hex_split[[1]][1] == "#") {
-    for (i in seq(2,7)) {
-      if ((str_detect(hex_split[[1]][i], "[O-f]")) == FALSE){
-        return (FALSE)
-      } else{
-        return(TRUE)
-      }
-    }
-  } else {
-     return(FALSE)
+  } else if (((num == 7) == TRUE) & (str_detect(hex2, "#[0-f][0-f][0-f][0-f][0-f][0-f]")) == TRUE) {
+    return(TRUE)
+  } else{
+    return(FALSE)
+  }
 }
-}
+
 
 #' @title is_hex_alpha()
 #' @description checks whether an input string is a valid color in
 #' hexadecimal notation with an alpha transparency
-#' @param x a string
+#' @param hexa a string
 #' @return TRUE or FALSE
 
-is_hex_alpha = function(x) {
-  
+is_hex_alpha = function(hexa) {
+  num = nchar(hexa)
+  hexa2 = as.vector((tolower(hexa)))
+  if (typeof(hexa) != "character") {
+    stop("invalid input; a string was expected")
+  } else if ((num == 9) == TRUE & (str_detect(hexa2, "#[0-f][0-f][0-f][0-f][0-f][0-f][0-f][0-f]")) == TRUE) {
+    return(TRUE)
+  } else{
+    return(FALSE)
+  }
 }
